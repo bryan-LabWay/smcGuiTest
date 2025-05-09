67ac9fcb0c98757fb602c68dec8d653722acf557
bryan-LabWay <bryan.o@labwaymath.com>
2025-05-09 08:11:52 +0800

    Adding new models for testing

--- setenv.js
+++ setenv.js
@@ -7,16 +7,21 @@ const envProdFilePath = join(__dirname, 'src', 'environments', 'environment.ts')
 // Get the environment variables from process.env
 const apiUrl_main = process.env.API_URL_MAIN;
 const apiKey_main = process.env.API_KEY_MAIN;
-const apiUrl_16Steps = process.env.API_URL_16STEPS;
 const apiUrl_3Epochs = process.env.API_URL_3EPOCHS;
+const apiUrl_6Epochs = process.env.API_URL_6EPOCHS;
+const apiUrl_9Epochs = process.env.API_URL_9EPOCHS;
+const apiUrl_18Epochs = process.env.API_URL_18EPOCHS;
 
 // Build the content of environment.prod.ts
 const envFileContent = `export const environment = {
   production: true,
   apiUrlMain: '${apiUrl_main}',
   apiKeyMain: '${apiKey_main}',
-  apiUrl16Steps: '${apiUrl_16Steps}',
   apiUrl3Epochs: '${apiUrl_3Epochs}'
+  apiUrl6Epochs: '${apiUrl_6Epochs}'
+  apiUrl9Epochs: '${apiUrl_9Epochs}'
+  apiUrl18Epochs: '${apiUrl_18Epochs}'
+
 };
 `;
 
