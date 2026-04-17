5bf72c5b8e5c6f680944c293156bddc497656c36
bordas <bryanbenedict.ordas21@gmail.com>
2026-04-17 09:44:29 +0800

    Adding v042

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -49,6 +49,7 @@ export class ChatComponent implements AfterViewChecked {
   modelConversations: { [modelName: string]: Message[] } = {};
 
   modelOptions = [
+    { name: 'v0.042', apiUrl: environment.apiUrlv042},
     { name: 'v0.041', apiUrl: environment.apiUrlv041},
     { name: 'v0.041-R1a', apiUrl: environment.apiUrlv041_R1a},
     { name: 'v0.041-R1b', apiUrl: environment.apiUrlv041_R1b},
